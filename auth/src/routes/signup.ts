3ff7a08a0a4ec7e8e24e1e2adddddb5002f1e62b
dexter <dexterdevmode@gmail.com>
2023-07-26 23:06:20 +0530

    Added funtion to add user in DB

--- auth/src/routes/signup.ts
+++ auth/src/routes/signup.ts
@@ -2,6 +2,8 @@ import { Router, Request, Response } from "express";
 import { body, validationResult } from "express-validator";
 import { RequestValidationError } from "../../errors/requestValidationError";
 import { DatabaseConnectionError } from "../../errors/databaseConnectionError";
+import { UserModel } from "../models/UserModel";
+import { GeneralError } from "../../errors/generalError";
 
 const router = Router();
 
@@ -11,17 +13,26 @@ router.post(
     body("email").isEmail().withMessage("Please enter a valid email"),
     body("password").trim().isLength({ min: 6 }).withMessage("Please enter a secure password"),
   ],
-  (req: Request, res: Response) => {
+  async (req: Request, res: Response) => {
     const errors = validationResult(req);
     if (!errors.isEmpty()) {
       throw new RequestValidationError(errors.array());
     }
 
-    try {
-      console.log("Creating a new User");
+    const { email, password } = req.body;
 
-      const { email, password } = req.body;
-      res.json({ user: email });
+    // console.log("Creating a new User");
+
+    const existingUser = await UserModel.findOne({ email: email });
+    if (existingUser) {
+      throw new GeneralError("User exists");
+    }
+
+    try {
+      const user = UserModel.build({ email, password });
+      await user.save();
+
+      res.status(201).json({ user });
     } catch (error) {
       throw new DatabaseConnectionError();
     }
