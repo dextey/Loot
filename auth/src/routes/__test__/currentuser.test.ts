effec2203035706e735a2d4a237d315e7f1bf128
dexter <dexterdevmode@gmail.com>
2023-08-19 12:50:32 +0530

    Added global signin functionality

--- auth/src/routes/__test__/currentuser.test.ts
+++ auth/src/routes/__test__/currentuser.test.ts
@@ -3,20 +3,20 @@ import { app } from "../../app";
 
 describe("CURRENT USER", () => {
   it("Get details of the user", async () => {
-    const postReq = await request(app)
-      .post("/api/users/signup")
-      .send({
-        email: "test@test.com",
-        password: "password",
-      })
-      .expect(201);
+    const cookie = await global.signin();
 
     const response = await request(app)
       .get("/api/users/currentuser")
-      .set("Cookie", postReq.get("Set-Cookie"))
+      .set("Cookie", cookie)
       .send()
       .expect(200);
 
     expect(response.body.currentUser.email === "test@test.com");
+  });
+
+  it("Returns null on not authenticated Request", async () => {
+    const response = await request(app).get("/api/users/currentuser").send().expect(200);
+
+    expect(response.body.currentUser).toBeNull();
   });
 });
--- auth/src/test/setup.ts
+++ auth/src/test/setup.ts
@@ -1,8 +1,13 @@
 import { MongoMemoryServer } from "mongodb-memory-server";
 import { app } from "../app";
 import mongoose from "mongoose";
+import request from "supertest";
 
 let mongo: any;
+
+declare global {
+  var signin: () => Promise<string[]>;
+}
 
 beforeAll(async () => {
   process.env.JWT_KEY = "Secret";
@@ -26,3 +31,13 @@ afterAll(async () => {
   }
   await mongoose.connection.close();
 });
+
+global.signin = async () => {
+  const email = "test@test.com";
+  const password = "password";
+
+  const response = await request(app).post("/api/users/signup").send({ email, password }).expect(201);
+
+  const cookie = response.get("Set-Cookie");
+  return cookie;
+};
