4328d8df7879c875eb552d82ef4de6cec0f4b3d4
dexter <dexterdevmode@gmail.com>
2023-09-15 16:04:11 +0530

    Added cors for local development

--- auth/src/app.ts
+++ auth/src/app.ts
@@ -6,17 +6,19 @@ import { signup } from "./routes/signup";
 import { signout } from "./routes/signout";
 import { handleError } from "../middlewares/errorHandler";
 import { NotFoundError } from "../errors/notFoundError";
-
+import cors from "cors";
 import cookieSession from "cookie-session";
 
 const app = express();
 app.set("trust proxy", true);
 app.use(express.json());
 
+app.use(cors());
+
 app.use(
   cookieSession({
     signed: false,
-    secure: process.env.NODE_ENV !== "test",
+    secure: false,
   })
 );
 
